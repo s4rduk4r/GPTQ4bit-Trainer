5e6bbc92dcba34fd6a59d71b3378e7fe586cb66b
s4rduk4r <s4rduk4r@gmail.com>
2023-04-09 11:50:30 +0300

    Refactor autograd

--- autograd_4bit/__init__.py
+++ autograd_4bit/__init__.py
@@ -5,6 +5,6 @@ from colorama import init, Fore, Style
 init(autoreset=True)
 
 # Module interface
-from .autograd_4bit_v2 import Autograd4bitQuantLinear
-from .autograd_4bit_v2 import load_llama_model_4bit_low_ram
-from .autograd_4bit_v2 import load_llama_model_4bit_low_ram_and_offload_to_cpu
+from .autograd_4bit import Autograd4bitQuantLinear
+from .autograd_4bit import load_llama_model_4bit_low_ram
+from .autograd_4bit import load_llama_model_4bit_low_ram_and_offload_to_cpu
