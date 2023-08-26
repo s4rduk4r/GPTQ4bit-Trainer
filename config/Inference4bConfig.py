13e05a5cf8a3a4f86280849621a5450698d60f74
s4rduk4r <s4rduk4r@gmail.com>
2023-08-27 00:39:56 +0300

    Add 'offload' and 'chat' options

--- config/Inference4bConfig.py
+++ config/Inference4bConfig.py
@@ -8,7 +8,9 @@ class Inference4bConfig:
                  llama_q4_config_dir: str, llama_q4_model: str,
                  lora_apply_dir: str,
                  groupsize: int,
+                 chat_mode: bool,
                  offloading: bool,
+                 offload_folder: str,
                  config_file_path: str,
                  device_map : str,
                  max_memory : dict
@@ -18,15 +20,19 @@ class Inference4bConfig:
             llama_q4_config_dir (str): Path to the config.json, tokenizer_config.json, etc
             llama_q4_model (str): Path to the quantized model in huggingface format
             lora_apply_dir (str): Path to directory from which LoRA has to be applied before training
+            chat_mode (bool): Start inference in chat mode
             groupsize (int): Group size of GPTQv2 model
             offloading (bool): Use offloading
+            offload_folder (str): Offloading disk folder
             config_file_path (str): path to config file used
         """
         self.llama_q4_config_dir = llama_q4_config_dir
         self.llama_q4_model = llama_q4_model
         self.lora_apply_dir = lora_apply_dir
+        self.chat_mode = chat_mode
         self.groupsize = groupsize
         self.offloading = offloading
+        self.offload_folder = offload_folder
         self.config_file_path = config_file_path
         self.device_map = device_map if device_map is not None else "auto"
         self.max_memory = None
@@ -44,6 +50,8 @@ class Inference4bConfig:
         s = f"\nParameters:\n{'Inference':-^20}\n" +\
         f"{self.llama_q4_config_dir=}\n{self.llama_q4_model=}\n{self.lora_apply_dir=}\n" +\
         f"{self.groupsize=}\n" +\
+        f"{self.chat_mode=}\n" +\
         f"{self.offloading=}\n" +\
+        f"{self.offload_folder=}\n" +\
         f"{self.config_file_path=}\n"
         return s.replace("self.", "")
