8dc07f92c94270688fe411fcd0658266c5c44fe3
s4rduk4r <s4rduk4r@gmail.com>
2023-08-28 01:24:10 +0300

    Hide chat implementation into class

--- inference.py
+++ inference.py
@@ -77,72 +77,116 @@ def get_model_response(prompt: str) -> str:
     return result_text
 
 
-def get_user_input() -> str:
-    """Simple CLI for chat. Supports single-line and multi-line input as well as some additional features
-    [HELP] - print help message
-    [RAW] - show raw chat history
-    [BEGIN] - start multiline input
-    [END] - end multiline input
+class LLMChat:
+    def __init__(self, get_model_response_function) -> None:
+        self.chat_history = None
+        self.get_model_response = get_model_response_function
 
-    Returns:
-        str: user input
-    """
-    is_multiline = False
+    def get_user_input(self) -> str:
+        """Simple CLI for chat. Supports single-line and multi-line input as well as some additional features
+        [HELP] - print help message
 
-    while True:
-        if not is_multiline:
-            print(Fore.LIGHTCYAN_EX + "USR:>", end="")
+        [RAW] - show raw chat history
 
-        input_line = input()
+        [BEGIN] - start multiline input
 
-        match(input_line):
-            case "":
-                continue
-            case "[HELP]":
-                print(Fore.LIGHTBLUE_EX + "Type your text to converse.\nMultiline mode ON: [BEGIN]\nMultiline mode OFF: [END]\nRaw chat history: [RAW]")
-                continue
-            case "[RAW]":
-                print(Fore.LIGHTGREEN_EX + chat_history)
-                continue
-            case "[BEGIN]":
-                user_text = ""
-                is_multiline = True
-                continue
-            case "[END]":
-                return user_text
-            case _:
-                if is_multiline:
-                    user_text += input_line + "\n"
-                else:
-                    return input_line
+        [END] - end multiline input
 
+        [NEW] - forget previous conversation and start a new one
+
+        Returns:
+            str: user input
+        """
+        is_multiline = False
+
+        while True:
+            if not is_multiline:
+                print(Fore.LIGHTCYAN_EX + "USR:>", end="")
+
+            input_line = input()
+
+            match(input_line):
+                case "":
+                    continue
+                case "[HELP]":
+                    print(Fore.LIGHTBLUE_EX + "Type your text to converse.\nMultiline mode ON: [BEGIN]\nMultiline mode OFF: [END]\nRaw chat history: [RAW]")
+                    continue
+                case "[RAW]":
+                    if self.chat_history is not None:
+                        print(Fore.LIGHTGREEN_EX + self.chat_history)
+                    continue
+                case "[NEW]":
+                    print(Fore.LIGHTRED_EX + "!!!New conversation started. All previous history is forgotten!!!")
+                    self.chat_history = None
+                    continue
+                case "[BEGIN]":
+                    user_text = ""
+                    is_multiline = True
+                    continue
+                case "[END]":
+                    return user_text
+                case _:
+                    if is_multiline:
+                        user_text += input_line + "\n"
+                    else:
+                        return input_line
+
+    def get_system_message(self) -> str:
+        """Simple CLI to get model system message
+
+        [HELP] - print help message
+
+        [BEGIN] - start multiline input
+
+        [END] - end multiline input
+
+        [DEFAULT] - use model's default system message
+
+        Returns:
+            str: system message string or None
+        """
+
+        is_multiline = False
+        while True:
+
+            if not is_multiline:
+                print(Fore.LIGHTYELLOW_EX + "Prompt System message:", end="")
+
+            input_txt = input()
+
+            match(input_txt):
+                case "[HELP]":
+                    print(Fore.LIGHTBLUE_EX + "Single-line input mode\nMultiline mode ON: [BEGIN]\nMultiline mode OFF: [END]\nDefault model system message: [DEFAULT]")
+                    continue
+                case "[BEGIN]":
+                    system_msg = ""
+                    is_multiline = True
+                    continue
+                case "[END]":
+                    return system_msg
+                case "[DEFAULT]":
+                    return None
+                case _:
+                    if is_multiline:
+                        system_msg += input_txt + "\n"
+                    else:
+                        return input_txt
+
+
+chat = LLMChat(get_model_response)
 
 from prompt_builders import Llama2ChatPrompt
-
-# Default system message
-prompt_system_msg = """You are a helpful, respectful and honest assistant. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature.
-
-If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don't know the answer to a question, please don't share false information."""
-
-
-system_msg = input(Fore.LIGHTYELLOW_EX + "Prompt System message:")
-if system_msg.startswith("+"):
-    system_msg = prompt_system_msg + system_msg.replace("+", "\n\n", 1)
-elif system_msg == "":
-    system_msg = prompt_system_msg
-
+system_msg = chat.get_system_message()
 prompt_builder = Llama2ChatPrompt(system_msg)
 
-chat_history = None
-
 while True:
-    user_text = get_user_input()
-    prompt = prompt_builder.make(user_text, chat_history)
+    user_text = chat.get_user_input()
+    prompt = prompt_builder.make(user_text, chat.chat_history)
 
     print("Processing ⌛", end="")
     start = time.time()
-    chat_history = get_model_response(prompt)
-    print("\r" + Fore.LIGHTMAGENTA_EX + "AI:>" + prompt_builder.refine_output(chat_history))
+    chat.chat_history = get_model_response(prompt)
+    print("\r" + Fore.LIGHTMAGENTA_EX + "AI:>" + prompt_builder.refine_output(chat.chat_history))
     end = time.time()
     print(Fore.LIGHTGREEN_EX + f"Inference time: {end - start:.2f}s")
 
